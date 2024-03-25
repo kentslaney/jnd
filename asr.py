34bf6aba934248f30645a0087408cba10b7ab29c
Kent Slaney <kent@slaney.org>
2024-03-25 16:26:44 -0700

    removed duplicated code

--- asr.py
+++ asr.py
@@ -11,22 +11,3 @@ normalizer = EnglishTextNormalizer()
 def asr(path):
     return normalizer(model.transcribe(path)["text"])
 
-import io
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-
-# https://stackoverflow.com/a/50728936/3476782
-def fig_bytes(fig) -> bytes:
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return output.getvalue()
-
-def scatter_fig(x, y):
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    axis.scatter(x, y)
-    return fig
-
-def results_png(x, y):
-    return fig_bytes(scatter_fig(x, y))
-
