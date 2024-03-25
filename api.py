2817295cb4f19a96840a904568f0f38a02936d09
Kent Slaney <kent@slaney.org>
2024-03-25 13:19:19 -0700

    OOP BP

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import json, sqlite3, unicodedata, uuid
 from flask import request, session
 from utils import relpath, DatabaseBP
 from pitch import PitchDB, PitchBP
-from quick import QuickDB, QuickBP
+from quick import QuickDB, QuickWhisperBP
 
 # use multiple inheritance to add other DB hooks
 class ExperimentDB(PitchDB, QuickDB):
@@ -16,7 +16,7 @@ class APIBlueprint(DatabaseBP):
         super().__init__(db_path, schema_path, name, url_prefix)
         db = lambda: self._blueprint_db
         self.register_blueprint(PitchBP(db))
-        self.register_blueprint(QuickBP(db))
+        self.register_blueprint(QuickWhisperBP(db))
         self._route_db("/username-available")(username_available)
         self._route_db("/set-username")(set_username)
         self._route_db("/authorized", methods=["POST"])(authorized)
--- quick.py
+++ quick.py
@@ -1,7 +1,6 @@
 import os, os.path, json, random
 from flask import Blueprint, request, session, abort, redirect, Response
 from utils import Database, relpath, DatabaseBP
-from werkzeug.wrappers import Response
 from matplotlib import pyplot
 
 quick_levels = 6
@@ -30,104 +29,132 @@ class QuickDB(Database):
         assert quick_levels <= self.queryone(
             "SELECT MAX(level_number) FROM quick_trials WHERE active=1")[0]
 
-quick_keys = ("id", "snr", "level_number", "trial_number", "filename", "answer")
-quick_trial_dict = lambda v: dict(zip(quick_keys, v))
-quick_url = lambda v: v and "/jnd/quick/" + v
-quick_done = [0, 0, 0, 1, "", 1]
-
-def quick_next(db, cur, done = False):
-    left = json.loads(session["left"])
-    session["left"] = json.dumps(left - 1)
-    if left == 0 or done:
-        q = quick_done
-    else:
-        q = db.queryall(
-            "SELECT * FROM quick_trials WHERE active=1 AND level_number=?",
-            (cur["level_number"] + 1,))
-        if len(q) == 0:
-            abort(Reponse("out of levels", code=400))
-        q = random.choice(q)
-    q = quick_trial_dict(q)
-    session["q"] = json.dumps(q)
-    return quick_url(q["filename"])
-
-def quick_start(db):
-    if "user" not in session:
-        abort(400)
-    elif "cur" in session:
-        cur, q = map(lambda x: quick_url(json.loads(x)["filename"]), (
-            session["cur"], session["q"]))
-        return json.dumps({
-            "cur": cur, "next": {1: q}, "name": session["username"],
-            "has_results": json.loads(session["left"]) < quick_levels - 1})
-    cur = db.queryall(
-        "SELECT * FROM quick_trials WHERE active=1 AND level_number=1")
-    if len(cur) == 0:
-        abort(400)
-    cur = quick_trial_dict(random.choice(cur))
-    session["cur"], session["left"] = json.dumps(cur), json.dumps(quick_levels)
-    return json.dumps({
-        "cur": quick_url(cur["filename"]), "next": {1: quick_next(db, cur)},
-        "name": session["username"], "has_results": False})
-
-def quick_result(db):
-    if "user" not in session or "file" not in request.files:
-        abort(400)
-    file = request.files["file"]
-    if file.filename == "":
-        abort(400)
-    cur = json.loads(session["cur"])
-    fname = f"sin_{session['user']}_{cur['id']}"
-    fpath = os.path.join(upload_location, fname)
-    file.save(fpath)
-    reply = asr(fpath)
-    db.execute(
-        "INSERT INTO quick_results "
-        "(subject, trial, reply_filename, reply_asr) VALUES (?, ?, ?, ?)",
-        (session["user"], cur["id"], fname, reply))
-    session["cur"] = session["q"]
-    return json.dumps({1: quick_next(
-        db, json.loads(session["cur"]),
-        completion_condition(reply, cur["answer"]))})
-
-# delayed by one level because of preloading
-def completion_condition(reply, answer):
-    return proportion_correct(reply, answer) == 0
-
-def proportion_correct(reply, answer):
-    reply = set(reply.split(" "))
-    correct, total = 0, 0
-    for options in answer.split(","):
-        for option in options.split("/"):
-            total += 1
-            if option in reply:
-                correct += 1
-    return correct / max(total, 1)
-
-def quick_plot(db):
-    results = db.queryall(
-        "SELECT quick_trials.snr, quick_results.reply_asr, "
-        "quick_trials.answer FROM quick_results LEFT JOIN quick_trials "
-        "ON quick_results.trial=quick_trials.id WHERE quick_results.subject=?",
-         (session["user"],))
-    if len(results) == 0:
-        abort(400)
-    results = [(snr, proportion_correct(*score)) for snr, *score in results]
-    png_bytes = results_png(*zip(*results))
-    return Response(png_bytes, mimetype='image/png')
-
 class QuickBP(DatabaseBP):
+    quick_keys = (
+        "id", "snr", "level_number", "trial_number", "filename", "answer")
+    quick_trial_dict = staticmethod(lambda v: dict(zip(QuickBP.quick_keys, v)))
+    quick_url = staticmethod(lambda v: v and "/jnd/quick/" + v)
+    quick_done = [0, 0, 0, 1, "", 1]
+
     def __init__(self, db, name="quick", url_prefix="/quick"):
-        # don't want to import (loads model) if BP isn't constructed
-        global asr, results_png
         Blueprint.__init__(self, name, __name__, url_prefix=url_prefix)
-        self._route_db("/start")(quick_start)
-        self._route_db("/result", methods=["POST"])(quick_result)
-        self._route_db("/plot")(quick_plot)
+        self._route_db("/start")(self.quick_start)
+        self._route_db("/result", methods=["POST"])(self.quick_result)
+        self._route_db("/plot")(self.quick_plot)
         self._bind_db = db
-        from asr import asr, results_png
 
     @property
     def _blueprint_db(self):
         return self._bind_db()
+
+    def quick_next(self, db, cur, done = False):
+        left = json.loads(session["left"])
+        session["left"] = json.dumps(left - 1)
+        if left == 0 or done:
+            q = self.quick_done
+        else:
+            q = db.queryall(
+                "SELECT * FROM quick_trials WHERE active=1 AND level_number=?",
+                (cur["level_number"] + 1,))
+            if len(q) == 0:
+                abort(Reponse("out of levels", code=400))
+            q = random.choice(q)
+        q = self.quick_trial_dict(q)
+        session["q"] = json.dumps(q)
+        return self.quick_url(q["filename"])
+
+    def quick_start(self, db):
+        if "user" not in session:
+            abort(400)
+        elif "cur" in session:
+            cur, q = map(lambda x: self.quick_url(json.loads(x)["filename"]), (
+                session["cur"], session["q"]))
+            return json.dumps({
+                "cur": cur, "next": {1: q}, "name": session["username"],
+                "has_results": json.loads(session["left"]) < quick_levels - 1})
+        cur = db.queryall(
+            "SELECT * FROM quick_trials WHERE active=1 AND level_number=1")
+        if len(cur) == 0:
+            abort(400)
+        cur = self.quick_trial_dict(random.choice(cur))
+        session["cur"] = json.dumps(cur)
+        session["left"] = json.dumps(quick_levels)
+        return json.dumps({
+            "cur": self.quick_url(cur["filename"]), "has_results": False,
+            "next": {1: self.quick_next(db, cur)}, "name": session["username"]})
+
+    def quick_result(self, db):
+        if "user" not in session or "file" not in request.files:
+            abort(400)
+        file = request.files["file"]
+        if file.filename == "":
+            abort(400)
+        cur = json.loads(session["cur"])
+        fname = f"sin_{session['user']}_{cur['id']}"
+        fpath = os.path.join(upload_location, fname)
+        file.save(fpath)
+        reply = self.asr(fpath)
+        db.execute(
+            "INSERT INTO quick_results "
+            "(subject, trial, reply_filename, reply_asr) VALUES (?, ?, ?, ?)",
+            (session["user"], cur["id"], fname, reply))
+        session["cur"] = session["q"]
+        return json.dumps({1: self.quick_next(
+            db, json.loads(session["cur"]),
+            self.completion_condition(reply, cur["answer"]))})
+
+    # delayed by one level because of preloading
+    def completion_condition(self, reply, answer):
+        return self.proportion_correct(reply, answer) == 0
+
+    def proportion_correct(self, reply, answer):
+        reply = set(reply.split(" "))
+        correct, total = 0, 0
+        for options in answer.split(","):
+            for option in options.split("/"):
+                total += 1
+                if option in reply:
+                    correct += 1
+        return correct / max(total, 1)
+
+    def quick_plot(self, db):
+        results = db.queryall(
+            "SELECT quick_trials.snr, quick_results.reply_asr, "
+            "quick_trials.answer FROM quick_results LEFT JOIN quick_trials "
+            "ON quick_results.trial=quick_trials.id "
+            "WHERE quick_results.subject=?", (session["user"],))
+        if len(results) == 0:
+            abort(400)
+        results = [(snr, self.proportion_correct(
+            *score)) for snr, *score in results]
+        return self.flask_png(*zip(*results))
+
+    @classmethod
+    def saved_png(cls, f, path):
+        def load_path(x, y):
+            f(x, y)
+            with open(path, 'rb') as fp:
+                return fp.read()
+        return cls.bytes_png(load_path)
+
+    @staticmethod
+    def bytes_png(f):
+        def flask_response(x, y):
+            return Response(f(x, y), mimetype='image/png')
+        return flask_response
+
+    @staticmethod
+    def asr(path):
+        raise NotImplementedError()
+
+    @staticmethod
+    def flask_png(x, y):
+        raise NotImplementedError()
+
+class QuickWhisperBP(QuickBP):
+    def __init__(self, *a, **kw):
+        super().__init__(*a, **kw)
+        # don't want to import (loads model) if BP isn't constructed
+        from asr import asr, results_png
+        self.asr, self.flask_png = asr, self.bytes_png(results_png)
 
