e1e02faca7f04c7b637d69617f249d60409a84dc
Kent Slaney <kent@slaney.org>
2024-03-28 20:23:31 -0700

    alt port command

--- server.sh
+++ server.sh
@@ -2,4 +2,5 @@ uwsgi --ini "$( dirname -- "$0" )/uwsgi.ini"
 # source env/bin/activate
 # nohup sh server.sh &
 # ps x | grep [s]erver | cut -f2 -d' ' | xargs kill && killall uwsgi
+# FLASK_APP="debug:app" flask run -p 8088
 
