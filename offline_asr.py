8143e80b8aa7e436a962209852ef1b124e176d12
Malcolm Slaney <malcolmslaney@gmail.com>
2025-01-18 14:44:35 -0800

    Adding option to change db file

--- offline_asr.py
+++ offline_asr.py
@@ -1,11 +1,14 @@
-import sqlite3, pathlib
 import json
+import os
+import pathlib
+import sqlite3
 from tqdm import tqdm
 
-basename = pathlib.Path(__file__).parents[0]
-con = sqlite3.connect(str(basename / "experiments.db"))
+import asr
 
-def quick_queue():
+basename = pathlib.Path(__file__).parents[0]
+
+def quick_queue(con: sqlite3.Connection):
     cur = con.execute(
             "SELECT quick_results.id, reply_filename, answer "
             "FROM quick_results "
@@ -16,16 +19,17 @@ def quick_queue():
     cur.close()
     return q
 
-def update(rowid, res):
+def update(con: sqlite3.Connection, rowid: int, res: str):
     res = json.dumps(res)
     cur = con.cursor()
     cur.execute("INSERT INTO quick_asr (ref, data) VALUES (?, ?)", (rowid, res))
     con.commit()
     cur.close()
 
-def main(asr):
-    for rowid, fname, ans in tqdm(quick_queue()):
-        update(rowid, asr(str(basename / "uploads" / fname)))
+def main(asr, db_file: str ='experiments.db'):
+    con = sqlite3.connect(os.path.join(basename, "experiments.db"))
+    for rowid, fname, ans in tqdm(quick_queue(con)):
+        update(con, rowid, asr(str(basename / "uploads" / fname)))
 
 def deduplicate(**kw):
     dup, sep = "json_extract(data, '$.' || ?) = ?", " AND "
@@ -48,6 +52,8 @@ if __name__ == "__main__":
     parser.add_argument("--model", default="large", choices=models, help=(
             "which whisper model size to use (default: large); see: "
             "https://github.com/openai/whisper#available-models-and-languages"))
+    parser.add_argument("--dbfile", default="experiments.db", 
+                        help="Which SQLite3 database file to process")
     parser.add_argument(
             "--prompted", dest="asr", default="WhisperASR",
             action="store_const", const="PromptedWhisperASR", help=(
@@ -55,9 +61,10 @@ if __name__ == "__main__":
                 "but can also bias results towards correct"))
     parser.add_argument("--force", action="store_true")
     args = parser.parse_args()
+
     if args.force:
         model_type = "default" if args.asr == "WhisperASR" else "prompted"
         deduplicate(model_name=args.model, model_type=model_type)
-    import asr
-    main(getattr(asr, args.asr)(args.model))
+    
+    main(getattr(asr, args.asr)(args.model, args.dbfile))
 
