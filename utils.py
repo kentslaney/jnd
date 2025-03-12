42116cb547f41b696b5462610a388205a6a948b2
Kent Slaney <kent@slaney.org>
2025-03-12 12:58:12 -0700

    blueprint inheritance

--- utils.py
+++ utils.py
@@ -80,3 +80,79 @@ class DatabaseBP(Blueprint):
     def _bind_db(self, app):
         self._blueprint_db = None
 
+def mangle(f):
+    f.mangle = True
+    return f
+
+def mangles(cls):
+    for i, j in zip(cls.__mro__[:-1], cls.__mro__[1:]):
+        for k in dir(i):
+            v = getattr(i, k)
+            if v == getattr(j, k, object()) or not hasattr(v, "mangle"):
+                continue
+            def closure(f):
+                prefix = lambda cls: cls.__name__.lower() + "_"
+                mangled = prefix(cls) + f.__name__
+                def wrapper(self, *a, **kw):
+                    stack = getattr(self, "mangler", None)
+                    self.mangler = cls
+                    res = f(self, *a, **kw)
+                    if stack is None:
+                        del self.mangler
+                    else:
+                        self.mangler = stack
+                    return res
+                wrapper.__name__ = mangled
+                setattr(i, mangled, wrapper)
+
+                def router(self, *a, **kw):
+                    if hasattr(self, "mangler"):
+                        return getattr(self, prefix(self.mangler) + f.__name__)(
+                                *a, **kw)
+                    return f(self, *a, **kw)
+                setattr(cls, k, router)
+            closure(v)
+    return cls
+
+'''
+class Parent:
+    @mangle
+    def a(self):
+        print(0, self.mangler.__name__, end=" ")
+
+    @mangle
+    def b(self):
+        print(3, end=" ")
+        self.a()
+
+class Intermediary(Parent):
+    @mangle
+    def a(self):
+        print(1, end=" ")
+        super().a()
+
+@mangles
+class Child0(Parent):
+    pass
+
+@mangles
+class Child1(Intermediary):
+    def child1_a(self):
+        print(2, end=" ")
+        super().child1_a()
+
+class Mixture(Child0, Child1):
+    pass
+
+obj = Mixture()
+
+print('child0_a')
+obj.child0_a() # 0 Child0
+print('\nchild0_b')
+obj.child0_b() # 3 0 Child0
+print('\nchild1_a')
+obj.child1_a() # 2 1 0 Child1
+print('\nchild1_b')
+obj.child1_b() # 3 2 1 0 Child1
+print()
+'''
