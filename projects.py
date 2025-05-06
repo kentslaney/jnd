7260f5cdb4941ebf4ea8d1ae5fd307c921111204
Malcolm Slaney <malcolm@ieee.org>
2025-05-05 19:17:45 -0700

    Adding correct qs 3dB file (#12)
    * Adding QuickSIN 3dB - Work in progress
    
    * fix functionality
    
    * missed one
    
    * fix instructions
    
    * Fixed errant parenthesis in main call.
    
    * Adding correct qs 3dB file
    
    * fix qs3
    
    * remove effort/score from qs3_done
    
    ---------
    
    Co-authored-by: Kent Slaney <kent@slaney.org>
    Co-authored-by: Kent Slaney <kentslaney@users.noreply.github.com>

--- projects.py
+++ projects.py
@@ -34,11 +34,20 @@ class Qs3Spec(AudioSpec):
     project_key = "qs3"
 
 class Qs3DB(Qs3Spec, AudioDB):
+    csv_keys = (
+        "active", "lang", "trial_number", "level_number", "snr", "filename",
+        "answer")
+
     def db_init_hook(self):
         super().db_init_hook()
         self.parse_csv(__class__)
 
 class Qs3BP(Qs3Spec, AudioOutputBP):
+    audio_keys = (
+        "id", "snr", "lang", "level_number", "trial_number", "filename",
+        "answer")
+    audio_done = [1, 0, "--", 0, 1, "", 1]
+
     def __init__(self, db, name="qs3", url_prefix="/qs3"):
         super().__init__(db, name, url_prefix)
 
